7ddb8711d5b8bc95e16ce9f5d2357280f16674e1
jonathanp2004 <jpadilla1782.2@gmail.com>
2026-04-08 11:13:25 -0500

    AllLess class and allLess method. The allLess method works by simply traversing through whole given String[] s, since the given max heap is based on length but lexicographical value, and these two do not correlate.

---
+++ src/AllLess.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class AllLess {
+
+    public static List<String> allLess(String[] s, int x){
+
+        List<String> results = new ArrayList<>();
+
+
+        for (int i = 0; i < s.length; i++) {
+
+            if( s[i] == ""|| s[i] == null){
+                if(x < 0){
+                    results.add(s[i]);
+
+                }
+            }else if(s[i].length() < x){
+                results.add(s[i]);
+
+            }
+
+        }
+        return results;
+    }
+
+    public static void main(String[] args){
+
+        AllLess main = new AllLess();
+
+        String[] s = {"zero", " size", "nutella", "jojo", "luna", "isse", "astor", "as", "entretien", null , "cal"};
+        int x = 6;
+
+        List results = allLess(s,x);
+
+        String r = results.toString();
+
+        System.out.println(r);
+
+    }
+
+}
