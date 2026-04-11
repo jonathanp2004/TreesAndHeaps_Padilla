98b2bca845e3c6ae98f0524945925741dc3b48b6
jonathanp2004 <jpadilla1782.2@gmail.com>
2026-04-11 16:21:57 -0500

    Created the genCode and genCodeHelper functions. The genCodeHelper does most fo the work by traversing the tree(in pre order) and building the code for each root node through recursion and appending each either a 0 or 1 to the current string inputted into the recursive call. Also created encode function  which loops through a given string and finds the code for the char at that iteration and appends it to the end of the current empty string. printStat function prints out frequencies, Huffman codes , and the encoded text.

--- src/Huffman.java
+++ src/Huffman.java
@@ -6,9 +6,13 @@ public class Huffman {
     private  HashMap<Character, Integer> frequencyMap;
     private String text;
     private HuffmanNode root;
+    private HashMap<Character, String> huffmanCodes = new HashMap<>();
 
     public Huffman(String text){
         this.text = text;
+        frequencyCount(text);
+        buildHuffman();
+        genCode();
     }
 
 
@@ -58,6 +62,58 @@ public class Huffman {
 
     }
 
+    public void genCode(){
+        genCodeHelper(root,"");
+    }
+
+    private  void genCodeHelper(HuffmanNode node, String code) {
+        if (node == null) return;
+
+        if (node.left == null && node.right == null) {
+            huffmanCodes.put(node.character, code);
+            return;
+        }
+        genCodeHelper(node.left, code + '0');
+        genCodeHelper(node.right, code + '1');
+    }
+
+    public String encode(String text){
+
+        String result = "";
+
+        for (int i = 0; i < text.length(); i++) {
+
+            char c1 = text.charAt(i);
+
+            if(huffmanCodes.containsKey(c1)){
+
+               result += huffmanCodes.get(i);
+
+            }
+
+        }
+        return result;
+    }
+
+    public void printStats(){
+
+        System.out.println("Frequencies: ");
+        var entrySet = frequencyMap.entrySet();
+        for(var entry : entrySet){
+            System.out.print(entry.getKey()+ ": " + entry.getValue());
+        }
+
+        System.out.println("Huffman Codes:");
+        var entrySet2 = huffmanCodes.entrySet();
+        for(var entry: entrySet2){
+            System.out.print(entry.getKey() + ": " + entry.getValue());
+        }
+
+        System.out.println("Encoded:");
+        System.out.println(encode(text));
 
 
+
+
+    }
 }
--- src/HuffmanNode.java
+++ src/HuffmanNode.java
@@ -1,6 +1,6 @@
 public class HuffmanNode implements Comparable<HuffmanNode> {
     private final int frequency;
-    private char character;
+    char character;
     HuffmanNode left, right;
 
     HuffmanNode(char character, int frequency){
@@ -19,5 +19,29 @@ public class HuffmanNode implements Comparable<HuffmanNode> {
 
     public int getFrequency() {
         return frequency;
+    }
+
+    public char getCharacter() {
+        return character;
+    }
+
+    public void setCharacter(char character) {
+        this.character = character;
+    }
+
+    public HuffmanNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(HuffmanNode left) {
+        this.left = left;
+    }
+
+    public HuffmanNode getRight() {
+        return right;
+    }
+
+    public void setRight(HuffmanNode right) {
+        this.right = right;
     }
 }
